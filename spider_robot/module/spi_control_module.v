4a9e87bda688d0794aaa7ebfd67cb6c479add00a
olichlika <olichlika@outlook.com>
2019-11-30 19:29:56 +0800

    spi pwm ok

--- spider_robot/module/spi_control_module.v
+++ spider_robot/module/spi_control_module.v
@@ -9,7 +9,21 @@ module spi_control_module(
 	
 	output reg [2:0] oLED_Sig,
 	
-	output reg [7:0] oPWM_Top_Left_Coxa_Control_Sig
+	output reg [7:0] oPWM_Top_Left_Coxa_Control_Sig,
+	output reg [7:0] oPWM_Top_Left_Femur_Control_Sig,
+	output reg [7:0] oPWM_Top_Left_Tibia_Control_Sig,
+
+	output reg [7:0] oPWM_Top_Right_Coxa_Control_Sig,
+	output reg [7:0] oPWM_Top_Right_Femur_Control_Sig,
+	output reg [7:0] oPWM_Top_Right_Tibia_Control_Sig,
+
+	output reg [7:0] oPWM_Back_Left_Coxa_Control_Sig,
+	output reg [7:0] oPWM_Back_Left_Femur_Control_Sig,
+	output reg [7:0] oPWM_Back_Left_Tibia_Control_Sig,
+
+	output reg [7:0] oPWM_Back_Right_Coxa_Control_Sig,
+	output reg [7:0] oPWM_Back_Right_Femur_Control_Sig,
+	output reg [7:0] oPWM_Back_Right_Tibia_Control_Sig
 );
 
 //************同步异步信号************
@@ -27,6 +41,20 @@ wire ncs_t = ncs_r[2];
 parameter LED_CONTROL = 8'd3;
 
 parameter TOP_LEFT_COXA_CONTROL = 8'd4;
+parameter TOP_LEFT_FEMUR_CONTROL = 8'd5;
+parameter TOP_LEFT_TIBIA_CONTROL = 8'd6;
+
+parameter TOP_RIGHT_COXA_CONTROL = 8'd7;
+parameter TOP_RIGHT_FEMUR_CONTROL = 8'd8;
+parameter TOP_RIGHT_TIBIA_CONTROL = 8'd9;
+
+parameter BACK_LEFT_COXA_CONTROL = 8'd10;
+parameter BACK_LEFT_FEMUR_CONTROL = 8'd11;
+parameter BACK_LEFT_TIBIA_CONTROL = 8'd12;
+
+parameter BACK_RIGHT_COXA_CONTROL = 8'd13;
+parameter BACK_RIGHT_FEMUR_CONTROL = 8'd14;
+parameter BACK_RIGHT_TIBIA_CONTROL = 8'd15;
 
 reg [7:0] i;
 
@@ -40,6 +68,20 @@ always @(posedge clk or negedge rst_n) begin
 			oLED_Sig <= 3'b000;
 			
 			oPWM_Top_Left_Coxa_Control_Sig <= 8'd0;
+			oPWM_Top_Left_Femur_Control_Sig <= 8'd0;
+			oPWM_Top_Left_Tibia_Control_Sig <= 8'd0;
+
+			oPWM_Top_Right_Coxa_Control_Sig <= 8'd0;
+			oPWM_Top_Right_Femur_Control_Sig <= 8'd0;
+			oPWM_Top_Right_Tibia_Control_Sig <= 8'd0;
+
+			oPWM_Back_Left_Coxa_Control_Sig <= 8'd0;
+			oPWM_Back_Left_Femur_Control_Sig <= 8'd0;
+			oPWM_Back_Left_Tibia_Control_Sig <= 8'd0;
+
+			oPWM_Back_Right_Coxa_Control_Sig <= 8'd0;
+			oPWM_Back_Right_Femur_Control_Sig <= 8'd0;
+			oPWM_Back_Right_Tibia_Control_Sig <= 8'd0;			
 		end
    else
 		if(!ncs_t)
@@ -61,7 +103,51 @@ always @(posedge clk or negedge rst_n) begin
 					else if(iData == 8'hA3)
 						begin
 							i <= TOP_LEFT_COXA_CONTROL;
-						end								
+						end
+					else if(iData == 8'hA4)
+						begin
+							i <= TOP_LEFT_FEMUR_CONTROL;
+						end
+					else if(iData == 8'hA5)
+						begin
+							i <= TOP_LEFT_TIBIA_CONTROL;
+						end			
+					else if(iData == 8'hA6)
+						begin
+							i <= TOP_RIGHT_COXA_CONTROL;
+						end
+					else if(iData == 8'hA7)
+						begin
+							i <= TOP_RIGHT_FEMUR_CONTROL;
+						end
+					else if(iData == 8'hA8)
+						begin
+							i <= TOP_RIGHT_TIBIA_CONTROL;
+						end
+					else if(iData == 8'hA9)
+						begin
+							i <= BACK_LEFT_COXA_CONTROL;
+						end
+					else if(iData == 8'hAA)
+						begin
+							i <= BACK_LEFT_FEMUR_CONTROL;
+						end
+					else if(iData == 8'hAB)
+						begin
+							i <= BACK_LEFT_TIBIA_CONTROL;
+						end			
+					else if(iData == 8'hAC)
+						begin
+							i <= BACK_RIGHT_COXA_CONTROL;
+						end
+					else if(iData == 8'hAD)
+						begin
+							i <= BACK_RIGHT_FEMUR_CONTROL;
+						end
+					else if(iData == 8'hAE)
+						begin
+							i <= BACK_RIGHT_TIBIA_CONTROL;
+						end						
 					else
 						i <= 8'd0;
 				2:
@@ -93,7 +179,109 @@ always @(posedge clk or negedge rst_n) begin
 							oPWM_Top_Left_Coxa_Control_Sig <= iData;
 						end
 					else
-						oCall <= 1'b1;					
+						oCall <= 1'b1;
+				TOP_LEFT_FEMUR_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Top_Left_Femur_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;
+				TOP_LEFT_TIBIA_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Top_Left_Tibia_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;	
+
+				TOP_RIGHT_COXA_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Top_Right_Coxa_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;
+				TOP_RIGHT_FEMUR_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Top_Right_Femur_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;
+				TOP_RIGHT_TIBIA_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Top_Right_Tibia_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;	
+				//-----后腿-----
+				BACK_LEFT_COXA_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Back_Left_Coxa_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;				
+				BACK_LEFT_FEMUR_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Back_Left_Femur_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;
+				BACK_LEFT_TIBIA_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Back_Left_Tibia_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;	
+
+				BACK_RIGHT_COXA_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Back_Right_Coxa_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;
+				BACK_RIGHT_FEMUR_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Back_Right_Femur_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;
+				BACK_RIGHT_TIBIA_CONTROL:
+					if(iDone[1])
+						begin
+							oCall <= 1'b0;
+							i <= 8'd0;
+							oPWM_Back_Right_Tibia_Control_Sig <= iData;
+						end
+					else
+						oCall <= 1'b1;				
 			endcase
 		else
 			begin
--- spider_robot/module/spi_slave_module.v
+++ spider_robot/module/spi_slave_module.v
@@ -7,7 +7,21 @@ module spi_slave_module(
 	
 	output [2:0] oLED_Sig,
 	
-	output PWM_Top_Left_Coxa_Sig
+	output PWM_Top_Left_Coxa_Sig,
+	output PWM_Top_Left_Femur_Sig,
+	output PWM_Top_Left_Tibia_Sig,
+
+	output PWM_Top_Right_Coxa_Sig,
+	output PWM_Top_Right_Femur_Sig,
+	output PWM_Top_Right_Tibia_Sig,
+	
+	output PWM_Back_Left_Coxa_Sig,
+	output PWM_Back_Left_Femur_Sig,
+	output PWM_Back_Left_Tibia_Sig,
+
+	output PWM_Back_Right_Coxa_Sig,
+	output PWM_Back_Right_Femur_Sig,
+	output PWM_Back_Right_Tibia_Sig		
 );
 
 wire [1:0] DoneU1/*synthesis keep*/;
@@ -30,6 +44,20 @@ wire CallU2;
 wire [7:0] DataU2;
 
 wire [7:0] PWM_Top_Left_Coxa_Control_Sig;
+wire [7:0] PWM_Top_Left_Femur_Control_Sig;
+wire [7:0] PWM_Top_Left_Tibia_Control_Sig;
+
+wire [7:0] PWM_Top_Right_Coxa_Control_Sig;
+wire [7:0] PWM_Top_Right_Femur_Control_Sig;
+wire [7:0] PWM_Top_Right_Tibia_Control_Sig;
+	
+wire [7:0] PWM_Back_Left_Coxa_Control_Sig;
+wire [7:0] PWM_Back_Left_Femur_Control_Sig;
+wire [7:0] PWM_Back_Left_Tibia_Control_Sig;
+
+wire [7:0] PWM_Back_Right_Coxa_Control_Sig;
+wire [7:0] PWM_Back_Right_Femur_Control_Sig;
+wire [7:0] PWM_Back_Right_Tibia_Control_Sig;
 
 spi_control_module U2(
 	.clk (clk),
@@ -41,14 +69,104 @@ spi_control_module U2(
 	.oData (DataU2),
 	.oLED_Sig (oLED_Sig),
 	
-	.oPWM_Top_Left_Coxa_Control_Sig (PWM_Top_Left_Coxa_Control_Sig)
+	.oPWM_Top_Left_Coxa_Control_Sig (PWM_Top_Left_Coxa_Control_Sig),
+	.oPWM_Top_Left_Femur_Control_Sig (PWM_Top_Left_Femur_Control_Sig),
+	.oPWM_Top_Left_Tibia_Control_Sig (PWM_Top_Left_Tibia_Control_Sig),
+	
+	.oPWM_Top_Right_Coxa_Control_Sig (PWM_Top_Right_Coxa_Control_Sig),
+	.oPWM_Top_Right_Femur_Control_Sig (PWM_Top_Right_Femur_Control_Sig),
+	.oPWM_Top_Right_Tibia_Control_Sig (PWM_Top_Right_Tibia_Control_Sig),
+	
+	.oPWM_Back_Left_Coxa_Control_Sig (PWM_Back_Left_Coxa_Control_Sig),
+	.oPWM_Back_Left_Femur_Control_Sig (PWM_Back_Left_Femur_Control_Sig),
+	.oPWM_Back_Left_Tibia_Control_Sig (PWM_Back_Left_Tibia_Control_Sig),
+	
+	.oPWM_Back_Right_Coxa_Control_Sig (PWM_Back_Right_Coxa_Control_Sig),
+	.oPWM_Back_Right_Femur_Control_Sig (PWM_Back_Right_Femur_Control_Sig),
+	.oPWM_Back_Right_Tibia_Control_Sig (PWM_Back_Right_Tibia_Control_Sig)
 );
-
-pwm PWM_Coxa_Module(
+//左前脚
+pwm PWM_Top_Left_Coxa_Module(
 	.clk (clk),
 	.rst_n (rst_n),
 	.iPWM_Control_Sig (PWM_Top_Left_Coxa_Control_Sig),
-	.oPWM_Output_Sig (PWM_Top_Left_Coxa_Sig)
+	.oPWM_Output_Sig (PWM_Top_Left_Coxa_Sig),
 );
 
+pwm PWM_Top_Left_Femur_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Top_Left_Femur_Control_Sig),
+	.oPWM_Output_Sig (PWM_Top_Left_Femur_Sig),
+);
+
+pwm PWM_Top_Left_Tibia_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Top_Left_Tibia_Control_Sig),
+	.oPWM_Output_Sig (PWM_Top_Left_Tibia_Sig),
+);	
+//右前脚
+pwm PWM_Top_Right_Coxa_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Top_Right_Coxa_Control_Sig),
+	.oPWM_Output_Sig (PWM_Top_Right_Coxa_Sig),
+);
+
+pwm PWM_Top_Right_Femur_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Top_Right_Femur_Control_Sig),
+	.oPWM_Output_Sig (PWM_Top_Right_Femur_Sig),
+);
+
+pwm PWM_Top_Right_Tibia_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Top_Right_Tibia_Control_Sig),
+	.oPWM_Output_Sig (PWM_Top_Right_Tibia_Sig),
+);
+//左后脚
+pwm PWM_Back_Left_Coxa_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Back_Left_Coxa_Control_Sig),
+	.oPWM_Output_Sig (PWM_Back_Left_Coxa_Sig),
+);
+
+pwm PWM_Back_Left_Femur_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Back_Left_Femur_Control_Sig),
+	.oPWM_Output_Sig (PWM_Back_Left_Femur_Sig),
+);
+
+pwm PWM_Back_Left_Tibia_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Back_Left_Tibia_Control_Sig),
+	.oPWM_Output_Sig (PWM_Back_Left_Tibia_Sig),
+);	
+//右后脚
+pwm PWM_Back_Right_Coxa_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Back_Right_Coxa_Control_Sig),
+	.oPWM_Output_Sig (PWM_Back_Right_Coxa_Sig),
+);
+
+pwm PWM_Back_Right_Femur_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Back_Right_Femur_Control_Sig),
+	.oPWM_Output_Sig (PWM_Back_Right_Femur_Sig),
+);
+
+pwm PWM_Back_Right_Tibia_Module(
+	.clk (clk),
+	.rst_n (rst_n),
+	.iPWM_Control_Sig (PWM_Back_Right_Tibia_Control_Sig),
+	.oPWM_Output_Sig (PWM_Back_Right_Tibia_Sig),
+);	
 endmodule
